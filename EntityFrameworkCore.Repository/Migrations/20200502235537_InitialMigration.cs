885c0fb703c6fdbb8dbba90b59001c96148e3d36
Niraj Patel <nirajp82@yahoo.com>
2020-05-02 20:04:39 -0400

    Migrated changes to database

---
+++ EntityFrameworkCore.Repository/Migrations/20200502235537_InitialMigration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFrameworkCore.Repository.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<Guid>(nullable: false, comment: "Unique id of a student"),
+                    FirstName = table.Column<string>(maxLength: 50, nullable: false),
+                    MiddleInitial = table.Column<string>(maxLength: 1, nullable: true),
+                    LastName = table.Column<string>(maxLength: 50, nullable: false),
+                    Age = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "idxStudentLFName",
+                table: "Students",
+                columns: new[] { "LastName", "FirstName" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}

f259d540af9196c5db276e72d355153dd6557e2e
Niraj Patel <nirajp82@yahoo.com>
2020-05-07 18:58:10 -0400

    Completed Updated Code

--- EntityFrameworkCore.Nucleus/Implementation/StudentEngine.cs
+++ EntityFrameworkCore.Nucleus/Implementation/StudentEngine.cs
@@ -3,6 +3,9 @@ using EntityFrameworkCore.DataModel;
 using EntityFrameworkCore.Repository;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace EntityFrameworkCore.Nucleus
@@ -75,26 +78,101 @@ namespace EntityFrameworkCore.Nucleus
             UpdateEvaluation(dbStudent, entity);
         }
 
-        private static void UpdateStudentAddress(Student dbStudent, StudentEntity entity)
+        private void UpdateStudentAddress(Student dbStudent, StudentEntity entity)
         {
-            if (!string.IsNullOrWhiteSpace(entity.Address))
+            if (string.IsNullOrWhiteSpace(entity.Address))
             {
-                dbStudent.StudentAddress = dbStudent.StudentAddress ?? new StudentAddress();
-                dbStudent.StudentAddress.Address = entity.Address;
-            }
-            else //Delete an Student Address
+                //Delete an Student Address
                 dbStudent.StudentAddress = null;
+                return;
+            }
+            dbStudent.StudentAddress = dbStudent.StudentAddress ?? new StudentAddress();
+            dbStudent.StudentAddress.Address = entity.Address;
         }
 
-        private static void UpdateEnrollment(Student dbStudent, StudentEntity entity)
+        private void UpdateEnrollment(Student dbStudent, StudentEntity entity)
         {
-           
+            //Remove all Student Subjects
+            if (entity.StudentSubjects == null)
+            {
+                dbStudent.StudentSubjects = null;
+                return;
+            }
+
+            //Add All Student subjects
+            if (dbStudent.StudentSubjects == null)
+            {
+                dbStudent.StudentSubjects = _mapperHelper.MapList<StudentSubjectEntity, StudentSubject>(entity.StudentSubjects).ToList();
+                return;
+            }
+
+            ManageStudentSubjects(dbStudent, entity);
         }
 
-        private static void UpdateEvaluation(Student dbStudent, StudentEntity entity)
+        private void UpdateEvaluation(Student dbStudent, StudentEntity entity)
         {
+            //Remove all Student Subjects
+            if (entity.Evaluations == null)
+            {
+                dbStudent.Evaluations = null;
+                return;
+            }
 
+            //Add All Student subjects
+            if (dbStudent.Evaluations == null)
+            {
+                dbStudent.Evaluations = _mapperHelper.MapList<EvaluationEntity, Evaluation>(entity.Evaluations).ToList();
+                return;
+            }
+
+            ManageEvalutions(dbStudent, entity);
         }
+
+        private void ManageStudentSubjects(Student dbStudent, StudentEntity entity)
+        {
+            IEnumerable<long> dbSubIdList = dbStudent.StudentSubjects.Select(ss => ss.SubjectId).ToList();
+            IEnumerable<long> entitySubIdList = entity.StudentSubjects.Select(ss => ss.SubjectId);
+
+            //Remove subjects.
+            foreach (var dbSubId in dbSubIdList.Where(dss => !entitySubIdList.Contains(dss)))
+            {
+                dbStudent.StudentSubjects.Remove(dbStudent.StudentSubjects.FirstOrDefault(ss => ss.SubjectId == dbSubId));
+            }
+
+            //Add newly added subjects
+            foreach (var eSubId in entitySubIdList.Where(ess => !dbSubIdList.Contains(ess)))
+            {
+                StudentSubject studentSubject = _mapperHelper.Map<StudentSubjectEntity, StudentSubject>(entity.StudentSubjects.FirstOrDefault(s => s.SubjectId == eSubId));
+                dbStudent.StudentSubjects.Add(studentSubject);
+            }
+        }
+
+        private void ManageEvalutions(Student dbStudent, StudentEntity entity)
+        {
+            IEnumerable<long> dbEvalList = dbStudent.Evaluations.Select(ss => ss.Id).ToList();
+            IEnumerable<long> entityEvalIdList = entity.Evaluations.Select(ss => ss.Id);
+
+            //Remove Evaluations.
+            foreach (var dbEvalId in dbEvalList.Where(dss => !entityEvalIdList.Contains(dss)))
+                dbStudent.Evaluations.Remove(dbStudent.Evaluations.FirstOrDefault(ss => ss.Id == dbEvalId));
+
+            //Add/Update Evaluations
+            foreach (var evaluationEntity in entity.Evaluations)
+            {                
+                if (evaluationEntity.Id == 0)
+                    dbStudent.Evaluations.Add(_mapperHelper.Map<EvaluationEntity, Evaluation>(evaluationEntity));
+                else
+                {
+                    var dbEvaluation = dbStudent.Evaluations.FirstOrDefault(e => e.Id == evaluationEntity.Id);
+                    if (dbEvaluation != null)
+                    {
+                        dbEvaluation.Grade = evaluationEntity.Grade;
+                        dbEvaluation.AdditionalExplanation = evaluationEntity.AdditionalExplanation;
+                    }
+                }
+            }
+        }
+
         #endregion
     }
 }
